af1d6f8c569f3e019bdba25e3dd5ea6ac2afe10c
Dorian Bucaille <74455265+dorian-bucaille@users.noreply.github.com>
2021-02-17 22:34:42 +0100

    Changed the cone display from lines to a Polygon Added comments

--- GreenSa/GreenSa.Android/CustomMapRenderer.cs
+++ GreenSa/GreenSa.Android/CustomMapRenderer.cs
@@ -23,6 +23,7 @@ using GreenSa.Models.GolfModel;
 using GreenSa.ViewController.Play.Game;
 
 using Geodesy;
+using System.Threading;
 
 [assembly: ExportRenderer(typeof(CustomMap), typeof(CustomMapRenderer))]
 namespace Greensa.Droid
@@ -31,13 +32,10 @@ namespace Greensa.Droid
     {
 
         GoogleMap map;
-        private Android.Gms.Maps.Model.Polyline targetLine;//the current polyline
-        private List<Android.Gms.Maps.Model.Polyline> coneLines;
-        private Android.Gms.Maps.Model.Circle circle;
-        private Android.Gms.Maps.Model.Circle circleMin;
-        private Android.Gms.Maps.Model.Circle circleMax;
-
-
+        private Android.Gms.Maps.Model.Polyline targetLine;  // The line from the user pin to the target
+        private List<Android.Gms.Maps.Model.Polyline> coneLines;  // The lines from the shot cone
+        private Android.Gms.Maps.Model.Circle circle;  // The range circle
+        private Android.Gms.Maps.Model.Polygon triangle;  // The shot triangle
 
         public CustomMapRenderer(Context context) : base(context){
             coneLines = new List<Android.Gms.Maps.Model.Polyline>();
@@ -45,7 +43,8 @@ namespace Greensa.Droid
                 try
                 {
                     UpdatePolyLinePos(false);
-                    UpdateShotCone(Math.PI / 4);
+                    UpdateShotTriangle();
+                    // UpdateShotCone(Math.PI / 4);
                 }
                 catch(Exception e) { }
             });
@@ -134,8 +133,10 @@ namespace Greensa.Droid
             return marker;
         }
 
+        // Updates the position of the shot cone
         public void UpdateShotCone(double angle)
         {
+            // If cone lines are currently displayed, remove them
             if (coneLines.Count != 0)
             {
                 foreach (Android.Gms.Maps.Model.Polyline line in coneLines)
@@ -145,32 +146,31 @@ namespace Greensa.Droid
             }
 
             CustomMap customMap = (CustomMap)this.Element;
-            GeodeticCalculator geoCalculator = new GeodeticCalculator();
-            double distTarget = 0.0;
             LatLng userPos = new LatLng(customMap.UserPin.Position.Latitude, customMap.UserPin.Position.Longitude);
 
             if (customMap != null)
-            {   
-                distTarget = customMap.getDistanceUserTarget();  // In meters
-                addConePolyline(-angle, geoCalculator, customMap, userPos, distTarget);  // Draws one part of the cone
-                addConePolyline(angle, geoCalculator, customMap, userPos, distTarget);  // Draws the other part of the cone
+            {
+                // DrawTriangle();
+                addConePolyline(-angle, customMap, userPos);  // Draws one part of the cone
+                addConePolyline(angle, customMap, userPos);  // Draws the other part of the cone
             }
         }
 
-        // Draws a cone's side
-        private void addConePolyline(double angle, GeodeticCalculator geoCalculator, CustomMap customMap, LatLng userPos, double distTarget)
+        // Add a cone's side to the variable coneLines
+        private void addConePolyline(double angle, CustomMap customMap, LatLng userPos)
         {
+            // The coordinates of the end of the side to be drawn
+            LatLng conePoint = movePoint(angle, customMap.UserPin.Position, customMap.TargetPin.Position);
+
             var polylineOptions = new PolylineOptions();
-            // polylineOptions.Clickable(true);
-            // polylineOptions.InvokeJointType(JointType.Round);
+
             polylineOptions.InvokeWidth(10f);
-            // polylineOptions.InvokeColor(0x664444FF); // Blue
             polylineOptions.InvokeColor(Android.Graphics.Color.Argb(240, 255, 20, 147)); // Pink
 
             polylineOptions.Add(userPos);
-            LatLng conePoint = movePoint(angle, customMap.UserPin.Position, customMap.TargetPin.Position);
-            Console.WriteLine("conePoint dist = " + CustomMap.DistanceTo(customMap.UserPin.Position.Latitude, customMap.UserPin.Position.Longitude, conePoint.Latitude, conePoint.Longitude, "M"));
             polylineOptions.Add(conePoint);
+
+            // Add the line to coneLines
             coneLines.Add(map.AddPolyline(polylineOptions));
         }
 
@@ -220,6 +220,7 @@ namespace Greensa.Droid
             }
         }
 
+        // Updates the position of the circle
         public void updateCircle()
         {
             CustomMap customMap = (CustomMap)this.Element;
@@ -246,9 +247,43 @@ namespace Greensa.Droid
             }
         }
 
+        // Makes the circle visible
         public void setCircleVisible(bool visible)
         {
             circle.Visible = visible;
         }
-      }
+
+
+        public void UpdateShotTriangle()
+        {
+            // If triangle is currently displayed, remove it
+            if (triangle != null)
+            {
+                triangle.Remove();
+            }
+
+            CustomMap customMap = (CustomMap)this.Element;
+
+            // Re-draw the triangle
+            if (customMap != null)
+            {
+                DrawTriangle();
+            }
+        }
+
+        // TEST ZONE //
+        // Draws a triangle in front of the player
+        private void DrawTriangle()
+        {
+            CustomMap customMap = (CustomMap)this.Element;
+
+            LatLng userPos = new LatLng(customMap.UserPin.Position.Latitude, customMap.UserPin.Position.Longitude);
+            LatLng pointA = movePoint(Math.PI / 4, customMap.UserPin.Position, customMap.TargetPin.Position);
+            LatLng pointB = movePoint(-Math.PI / 4, customMap.UserPin.Position, customMap.TargetPin.Position);
+            this.triangle = map.AddPolygon(new PolygonOptions()
+                .Add(userPos, pointA, pointB)
+                .InvokeFillColor(Android.Graphics.Color.Argb(150, 255, 0, 0)));
+        }
+
+    }
 }
